ec8f5362c3851cc169c6fc1896c314af376654e1
nOSkr <118327643+nOSkr@users.noreply.github.com>
2023-04-05 16:18:24 +0800

    a

--- controller/articles.js
+++ controller/articles.js
@@ -9,7 +9,7 @@ const User = require("../models/User");
 // api/v1/articles
 exports.getArticles = asyncHandler(async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 5;
+  const limit = parseInt(req.query.limit) || 100;
   const sort = req.query.sort;
   const select = req.query.select;
 
