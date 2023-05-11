73287da2703b402e4dd213306917c20fb0d7525c
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-05-11 15:21:13 +0800

    a

--- server.js
+++ server.js
@@ -43,15 +43,15 @@ var whitelist = [
 // Өөр домэйн дээр байрлах клиент вэб аппуудаас шаардах шаардлагуудыг энд тодорхойлно
 var corsOptions = {
   // Ямар ямар домэйнээс манай рест апиг дуудаж болохыг заана
-  origin: function (origin, callback) {
-    if (origin === undefined || whitelist.indexOf(origin) !== -1) {
-      // Энэ домэйнээс манай рест рүү хандахыг зөвшөөрнө
-      callback(null, true);
-    } else {
-      // Энэ домэйнд хандахыг хориглоно.
-      callback(new Error("Horigloj baina.."));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+  //     // Энэ домэйнээс манай рест рүү хандахыг зөвшөөрнө
+  //     callback(null, true);
+  //   } else {
+  //     // Энэ домэйнд хандахыг хориглоно.
+  //     callback(new Error("Horigloj baina.."));
+  //   }
+  // },
   // Клиент талаас эдгээр http header-үүдийг бичиж илгээхийг зөвшөөрнө
   allowedHeaders: "Authorization, Set-Cookie, Content-Type",
   // Клиент талаас эдгээр мэссэжүүдийг илгээхийг зөвөөрнө
