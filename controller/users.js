600b835b10d9cdd34045b5ec18ec52a880065c28
nOSkr <122243978+nOSkr04@users.noreply.github.com>
2023-05-21 03:44:01 +0800

    a

--- controller/users.js
+++ controller/users.js
@@ -253,18 +253,13 @@ exports.invoiceCheck = asyncHandler(async (req, res) => {
 });
 
 exports.chargeTime = asyncHandler(async (req, res, next) => {
-  console.log(req.params.id, "aaa");
-  console.log(req.params.numId);
-  console.log(req.params.numId === "200" ? "a" : "b");
   const profile = await User.findById(req.params.id);
-  console.log(profile.deadline, "profile");
   if (profile.deadline < Date.now()) {
     if (req.params.numId === "150") {
       profile.deadline = Date.now() + 60 * 60 * 1000 * 24 * 30;
     } else if (req.params.numId === "15000") {
       profile.deadline = Date.now() + 60 * 60 * 1000 * 24 * 60;
     } else if (req.params.numId === "20000") {
-      console.log("first");
       profile.deadline = Date.now() + 60 * 60 * 1000 * 24 * 90;
     }
   } else {
